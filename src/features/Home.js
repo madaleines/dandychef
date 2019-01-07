df5c7d91d82b905d0cf73080b7f27cb70b9cb9f6
Madaleine Shields <maddiev.shields@gmail.com>
2019-01-06 21:35:31 -0800

    Upload image

--- src/features/Home.js
+++ src/features/Home.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { Image, Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { logout } from '../helpers/auth';
+import FileUploader from 'react-firebase-file-uploader';
+import firebase from 'firebase';
+
 const appTokenKey = "appToken";
 export default class Home extends Component {
+
   constructor(props) {
     super(props);
+
     const allPhotos = [
       {
         id: 'randomstringimadeup43454356546',
@@ -20,11 +25,16 @@ export default class Home extends Component {
         url: 'http://fillmurray.com/200/200'
       }
     ]
+
     this.state = {
       allPhotos
     };
+
     this.handleLogout = this.handleLogout.bind(this);
+
   }
+
+
   handleLogout() {
     logout()
     .then(() => {
@@ -33,9 +43,44 @@ export default class Home extends Component {
       console.log("user signed out from firebase");
     });
   }
+
+  async handleUploadSuccess (filename) {
+
+    try {
+      let { bucket, fullPath } = await firebase.storage().ref('images').child(filename).getMetadata();
+      console.log('bucket', bucket)
+      console.log('fullPath', fullPath)
+      let downloadURL = await firebase.storage().ref('images').child(filename).getDownloadURL();
+      console.log('downloadURL', downloadURL)
+
+      let { uid, email, displayName } = await firebase.auth().currentUser;
+
+      let newPhoto = {
+        url: downloadURL,
+        userName: displayName,
+        userId: uid,
+        email,
+        bucket,
+        fullPath
+      }
+      console.log('newPhoto', newPhoto);
+
+      await firebase.firestore().collection('photos').add(newPhoto);
+    }
+
+    catch(err) {
+      console.error(err);
+    }
+
+  }
+
+
+
   render() {
+
     // our doppelganger images
     const allImages = this.state.allPhotos.map(photo => {
+
       return (
         <div key={photo.id}>
         <div style={{minHeight: '215px'}}>
@@ -45,22 +90,39 @@ export default class Home extends Component {
         </div>
       );
     })
+
     return (
       <div>
       {allImages}
+
       <Grid className="bottom-nav">
       <Row className="show-grid">
       <Col xs={4} className="col-bottom">
       <Link to="/app/album"><i className="bottom-icon material-icons">collections</i></Link>
       </Col>
       <Col xs={4} className="col-bottom">
+
+      <label>
       <i className="bottom-icon material-icons">camera_alt</i>
+      <FileUploader
+      hidden
+      accept="image/*"
+      storageRef={firebase.storage().ref('images')}
+      onUploadStart={this.handleUploadStart}
+      onUploadError={this.handleUploadError}
+      onUploadSuccess={this.handleUploadSuccess}
+      onProgress={this.handleProgress}
+      />
+      </label>
+
+
       </Col>
       <Col onClick={this.handleLogout} xs={4} className="col-bottom">
       <i className="bottom-icon material-icons">assignment_return</i>
       </Col>
       </Row>
       </Grid>
+
       </div>
     );
   }
